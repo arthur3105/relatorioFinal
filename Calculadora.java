7e67a7970b6b9b08eb6e44ce4697cdbed449d70c
arthur3105 <107140505+arthur3105@users.noreply.github.com>
2022-06-10 01:42:50 -0300

    Add files via upload

--- Calculadora.java
+++ Calculadora.java
@@ -14,5 +14,21 @@ public class Calculadora {
 	public double multiplicacao(double a, double b) {
 		return a * b;
 	}
+
+	public double exponencial(double a, double b){
+		return Math.pow(a, b);
+	}
+
+	public double raizQuadrada(double a){
+		return Math.sqrt(a);
+	}
+
+	public double piso(double a){
+		return Math.floor(a);
+	}
+	
+	public double teto(double a){
+		return Math.ceil(a);
+	}
     
 }

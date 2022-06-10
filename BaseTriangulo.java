3e4af2e9f9cac671853587a744b0fd13c45b5754
arthur3105 <107140505+arthur3105@users.noreply.github.com>
2022-06-10 01:26:40 -0300

    Add files via upload

---
+++ BaseTriangulo.java
@@ -0,0 +1,9 @@
+public class BaseTriangulo {
+    Calculadora calculo = new Calculadora();
+    public double baseT (double base){
+        base = calculo.multiplicacao(a, b)/2;
+        System.out.println(base);
+
+    }
+    
+}
